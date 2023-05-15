a552758ccb14d18571cc56518a5086ec2ebd7721
Miguel G <migalvez2002@gmail.com>
2023-05-15 12:15:21 -0500

    Creacion de la app, parte de login construido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './modules/login/login.component';
+import { MoviesComponent } from './modules/movies/movies.component';
+import { MovieDetailComponent } from './modules/movies/movie-detail/movie-detail.component';
+import { BaseComponent } from './shared/base/base.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MoviesComponent,
+    MovieDetailComponent,
+    BaseComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    FormsModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/properties/properties.ts
@@ -0,0 +1,3 @@
+export const properties ={
+    logo: 'https://media.istockphoto.com/id/1368151370/es/foto/usuario-escribiendo-login-y-contrase%C3%B1a-concepto-de-seguridad-cibern%C3%A9tica.jpg?b=1&s=612x612&w=0&k=20&c=LJPGsyUFuCCzzp8m1oC2eqYINvV_LyN_baYyJ9MgytQ=', 
+}
